14cdad17dcb25bae9453a7b6611e75569ac3589d
lb <863379105@qq.com>
2021-12-31 00:37:23 +0800

    done:leetcode-590-n-叉树的后序遍历

---
+++ leetcode/leetcode-590-n-叉树的后序遍历.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=590 lang=javascript
+ *
+ * [590] N 叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var postorder = function(root) {
+    let result = []
+    postOrderTraverseNode(root,result)
+    return result
+};
+var postOrderTraverseNode = function(node,result) {
+  if(node) {
+    node.children.forEach(child => {
+      postOrderTraverseNode(child,result)
+    });
+    result.push(node.val)
+  }
+}
+// @lc code=end
+
